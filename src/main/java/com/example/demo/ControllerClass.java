c2cbd56da1cfd84aae56eb2dd936785561622bf5
Azim <abdulazimka@gmail.com>
2023-03-27 17:44:59 +0530

    One

---
+++ src/main/java/com/example/demo/ControllerClass.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ControllerClass {
+
+	@RequestMapping("/view")
+	public String display() {
+		return "Hi all, How are you?";
+	}
+	
+	@RequestMapping("/view/1")
+	public  String display2() {
+		return "I am fine";
+	}
+}
